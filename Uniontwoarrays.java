4e4511d6d7c4d018d2ef19e6d6372e853a4484a2
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-12 20:53:41 +0530

    Add files via upload

--- Uniontwoarrays.java
+++ Uniontwoarrays.java
@@ -19,10 +19,31 @@ public class Uniontwoarrays {
         return newarr;
     }
 
+    public static int[] sortedUnion(int arr1[], int arr2[]) {
+        int newarr[] = new int[arr1.length];
+        boolean flag = true;
+        int k = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    flag = false;
+                    break;
+                } else if (arr2[j] > arr1[i]) {
+                    break;
+                }
+            }
+            if (flag) {
+                newarr[k++] = arr1[i];
+            }
+            flag = true;
+        }
+        return newarr;
+    }
+
     public static void main(String[] args) {
         int a[] = { 1, 2, 3, 4, 5, 6 };
         int b[] = { 2, 6, 7, 8, 9 };
-        int c[] = unsortedUnion(a, b);
+        int c[] = sortedUnion(a, b);
         for (int i : c) {
             System.out.println(i);
         }
