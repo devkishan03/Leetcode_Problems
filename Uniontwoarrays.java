7c99956513f1cbbc2a583edde837d923bc3b5d2c
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-17 00:03:33 +0530

    Add files via upload

--- Uniontwoarrays.java
+++ Uniontwoarrays.java
@@ -1,51 +1,68 @@
 public class Uniontwoarrays {
 
     public static int[] unsortedUnion(int arr1[], int arr2[]) {
-        int newarr[] = new int[arr1.length];
-        boolean flag = true;
-        int i = 0, k = 0;
-        for (; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-                newarr[k++] = arr1[i];
-            flag = true;
-
-        }
-        return newarr;
-    }
-
-    public static int[] sortedUnion(int arr1[], int arr2[]) {
-        int newarr[] = new int[arr1.length];
+        int newarr[] = new int[arr1.length + arr2.length];
         boolean flag = true;
         int k = 0;
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
+            newarr[k++] = arr1[i];
+        }
+        int i = 0;
+        for (; i < arr2.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                if (arr2[i] == arr1[j]) {
                     flag = false;
-                    break;
-                } else if (arr2[j] > arr1[i]) {
                     break;
                 }
             }
             if (flag) {
-                newarr[k++] = arr1[i];
+                newarr[k++] = arr2[i];
             }
             flag = true;
         }
+
+        return newarr;
+    }
+
+    public static int[] sortedUnion(int arr1[], int arr2[]) {
+        int newarr[] = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] > arr2[j]) {
+                newarr[k++] = arr2[j++];
+            } else if (arr1[i] < arr2[j]) {
+                newarr[k++] = arr1[i++];
+            } else {
+                newarr[k++] = arr1[i++];
+                j++;
+            }
+
+        }
+        while (i < arr1.length) {
+            if (arr1[i] > newarr[k - 1]) {
+                newarr[k++] = arr1[i++];
+            } else {
+                i++;
+            }
+
+        }
+        while (j < arr2.length) {
+            if (arr2[j] > newarr[k - 1]) {
+                newarr[k++] = arr2[j++];
+            } else {
+                j++;
+            }
+        }
+
         return newarr;
     }
 
     public static void main(String[] args) {
-     //   int a[] = { 1, 2, 3, 4, 5, 6 };
-     //   int b[] = { 2, 6, 7, 8, 9 };
-     //   int c[] = sortedUnion(a, b);
-     //   for (int i : c) {
-      //      System.out.println(i);
-      //  }
+        int a[] = { 1, 2, 3, 4, 5, 6 };
+        int b[] = { 2, 6, 6, 7, 8, 9, 10, 11 };
+        int c[] = unsortedUnion(a, b);
+        for (int i : c) {
+            System.out.println(i);
+        }
     }
 }
